407dc212153c7f7d78e7051e8424a10536b27e0e
Your Name <greg.baker2@googlemail.com>
2024-05-30 15:27:30 +0100

    extracted some logic to the collidable component

--- src/components/collideable.rs
+++ src/components/collideable.rs
@@ -1,9 +1,22 @@
-use bevy::prelude::*;
+use crate::components::player::Player;
+use bevy::{
+        math::bounding::{Aabb2d, IntersectsVolume, RayCast2d},
+        math::primitives::Direction2d,
+        prelude::*,
+};
 
-#[derive(Component, Debug)]
+#[derive(Component, Debug, Clone, Copy)]
 pub struct Collideable {
         pub width: f32,
         pub height: f32,
+}
+
+#[derive(Debug)]
+pub struct Directions {
+        pub up: bool,
+        pub down: bool,
+        // left: bool,
+        // right: bool,
 }
 
 impl Collideable {
@@ -12,5 +25,77 @@ impl Collideable {
                         width: width as f32,
                         height: height as f32,
                 }
+        }
+
+        pub fn get_colliding_sides(
+                self,
+                self_pos: Vec3,
+                collidables_query: Query<(&Collideable, &Transform)>,
+        ) -> Directions {
+                let player_aabb = Aabb2d {
+                        min: Vec2 {
+                                x: self_pos.x - (self.width / 2.),
+                                y: self_pos.y - (self.height / 2.),
+                        },
+                        max: Vec2 {
+                                x: self_pos.x + (self.width / 2.),
+                                y: self_pos.y + (self.height / 2.),
+                        },
+                };
+
+                let mut colliding_sides = Directions {
+                        up: false,
+                        down: false,
+                        // left: false,
+                        // right: false,
+                };
+
+                for (collidable, transform) in collidables_query.iter() {
+                        let center = transform.translation;
+
+                        let collidable_aabb = Aabb2d {
+                                min: Vec2 {
+                                        x: center.x - (collidable.width / 2.),
+                                        y: center.y - (collidable.height / 2.),
+                                },
+                                max: Vec2 {
+                                        x: center.x + (collidable.width / 2.),
+                                        y: center.y + (collidable.height / 2.),
+                                },
+                        };
+
+                        if player_aabb.intersects(&collidable_aabb) {
+                                let pos_y_ray = RayCast2d::new(
+                                        Vec2 {
+                                                x: self_pos.x,
+                                                y: self_pos.y,
+                                        },
+                                        Direction2d::Y,
+                                        50.,
+                                );
+                                let neg_y_ray = RayCast2d::new(
+                                        Vec2 {
+                                                x: self_pos.x,
+                                                y: self_pos.y,
+                                        },
+                                        Direction2d::NEG_Y,
+                                        50.,
+                                );
+
+                                let pos_y_intersection =
+                                        pos_y_ray.aabb_intersection_at(&collidable_aabb);
+                                let neg_y_intersection =
+                                        neg_y_ray.aabb_intersection_at(&collidable_aabb);
+
+                                if Option::is_some(&pos_y_intersection) {
+                                        colliding_sides.up = true;
+                                }
+                                if Option::is_some(&neg_y_intersection) {
+                                        colliding_sides.down = true;
+                                }
+                        }
+                }
+
+                colliding_sides
         }
 }
